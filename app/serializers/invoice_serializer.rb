82e7d5e77a26dfb3d125d5586e0ecd515f887322
Jackson Bickler <jbickler4@gmail.com>
2025-01-20 14:12:57 -0700

    fixed_invoice_serializer

--- app/serializers/invoice_serializer.rb
+++ app/serializers/invoice_serializer.rb
@@ -1,5 +1,18 @@
 class InvoiceSerializer
   include JSONAPI::Serializer
   attributes :merchant_id, :customer_id, :status
-  belongs_to :coupon
+
+  attribute :coupon do |invoice|
+    if invoice.coupon.present?
+      {
+        id: invoice.coupon.id,
+        name: invoice.coupon.name,
+        code: invoice.coupon.code,
+        value_type: invoice.coupon.value_type,
+        value: invoice.coupon.value
+      }
+    else
+      nil
+    end
+  end
 end
