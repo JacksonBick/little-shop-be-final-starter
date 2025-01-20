b0d4609a65566ef0967527617f529df4d69d0cdf
Jackson Bickler <jbickler4@gmail.com>
2025-01-20 14:22:07 -0700

    wrraped_coupon_controoler

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -1,52 +1,52 @@
-class CouponsController < ApplicationController
-  before_action :set_coupon, only: [:show, :edit, :update, :destroy, :toggle_activation]
+module Api
+  module V1
+    class CouponsController < ApplicationController
+      before_action :set_coupon, only: [:show, :update, :destroy, :toggle_activation]
 
-  def index
-    @coupons = Coupon.all
-    render json: @coupons
-  end
+      def index
+        @coupons = Coupon.all
+        render json: @coupons
+      end
 
-  def show
-    render json: @coupon
-  end
+      def show
+        render json: @coupon
+      end
 
-  def new
-    @coupon = Coupon.new
-  end
+      def create
+        @coupon = Coupon.new(coupon_params)
+        if @coupon.save
+          render json: @coupon, status: :created
+        else
+          render json: @coupon.errors, status: :unprocessable_entity
+        end
+      end
 
-  def create
-    @coupon = Coupon.new(coupon_params)
-    if @coupon.save
-      render json: @coupon, status: :created
-    else
-      render json: @coupon.errors, status: :unprocessable_entity
+      def update
+        if @coupon.update(coupon_params)
+          render json: @coupon
+        else
+          render json: @coupon.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        render json: { message: 'Cannot delete a coupon. Only deactivate it.' }, status: :forbidden
+      end
+
+      def toggle_activation
+        @coupon.toggle_activation
+        render json: @coupon
+      end
+
+      private
+
+      def set_coupon
+        @coupon = Coupon.find(params[:id])
+      end
+
+      def coupon_params
+        params.require(:coupon).permit(:name, :code, :value_type, :value, :merchant_id)
+      end
     end
-  end
-
-  def update
-    if @coupon.update(coupon_params)
-      render json: @coupon
-    else
-      render json: @coupon.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    render json: { message: 'Cannot delete a coupon. Only deactivate it.' }, status: :forbidden
-  end
-
-  def toggle_activation
-    @coupon.toggle_activation
-    render json: @coupon
-  end
-
-  private
-
-  def set_coupon
-    @coupon = Coupon.find(params[:id])
-  end
-
-  def coupon_params
-    params.require(:coupon).permit(:name, :code, :value_type, :value, :merchant_id)
   end
 end
