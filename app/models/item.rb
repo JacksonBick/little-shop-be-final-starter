604cbe0264e421f943d3bd0db9fe7551c71c9e18
Jackson Bickler <jbickler4@gmail.com>
2025-01-21 14:01:46 -0700

    cleaning up the code

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :unit_price, presence: true, numericality: true
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
-  # has_many :invoices, through: :invoice_items
+
 
   def self.sort_by_price
     self.order(unit_price: :asc)
